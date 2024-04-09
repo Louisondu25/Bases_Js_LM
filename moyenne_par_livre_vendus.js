e1168c9b0fa77c74363fda33cdeeb25ac4c6bb2e
z bakura <louisondu25@gmail.com>
2024-04-09 11:14:34 +0200

    nouveaux exercices

---
+++ moyenne_par_livre_vendus.js
@@ -0,0 +1,5 @@
+//exercice 6
+let livrevendus = 100                                           //total des livres vendu par semaine
+livrevendus += 50                                               // ajout des livres vendu
+let moyennedelivrevendus = livrevendus / 7                      //calcul des livres vendus
+console.log("Le nombre de livre vendu par jour est de :" , moyennedelivrevendus,Math.round(moyennedelivrevendus))                         //affichage du commentaire + resultat

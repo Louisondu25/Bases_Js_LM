1b8f5a1469e0d6c11207ae418109f64fb6afd20b
z bakura <louisondu25@gmail.com>
2024-04-26 09:46:00 +0200

    Exercice des Fonctions

--- recursivite.js
+++ recursivite.js
@@ -59,19 +59,25 @@ for (var i=0; i <tab.length; i++) {
 
 recursiveLoopSub(people)
 
-function affichagefamille(tab, indent, origin){
-    let tabulation = ""
+function affichagefamille(tab, indent, origin){         // ajouter  la fonction "affichagefamille"
+    let tabulation = ""                                 // ouverture de la variable tabulation.
 
 
     for(y=0;y < indent; y++){   //tant que inferieur a indent
     tabulation += " "           // ajout d'espace dans tabulation.
     }
-    for(let i=0; i< tab.length; i++) {  //tant que i Inferieur a tab
-        console.log(`${tabulation} nom: ${tab[i].lastName} / prenom:${tab[i].firstName} / origin: ${origin}`)   // affichage console de tabulation,du nom a la positioni et de l'origine
-        if (tab [i].children && tab[i].children.length > 0) {  //si dans tab  a la position i il y a  le parametre enfant et presence d'enfant
-            affichagefamille(tab[i].children, indent + 1, origin + `${tab[i].firstName} ${tab[i].lastName} - `)         //appelle de la fonction affichage famille en donnant tableau enfant + augmentation indent + nom et prenom du parent
-
-        }
-
+    for(let i=0; i< tab.length; i++) {
+      //si i est  Inferieur a  la taille du tableau
+      console.log(
+        `${tabulation} nom: ${tab[i].lastName} / prenom:${tab[i].firstName} / origin: ${origin}`
+      ); // affichage  de la variable tabulation(donc l'espace), avec la position i du nom de famille dans le tableau /la position i du prenom dans le tableau et de l'origine.
+      if (tab[i].children && tab[i].children.length > 0) {
+        //si dans tableau enfant a la position i il y a  le parametre enfant et presence d'enfant
+        affichagefamille(
+          tab[i].children,
+          indent + 1,
+          origin + `${tab[i].firstName} ${tab[i].lastName} - `
+        ); //si le i élément du tableau tab a des enfants et si ces enfants ont une longueur supérieure à 0.
+      }
     }
 }

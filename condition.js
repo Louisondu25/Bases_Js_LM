03363fe291a953d270a7dbf3fc6567c86879d378
z bakura <louisondu25@gmail.com>
2024-05-01 15:04:15 +0200

    Exercice d'imbrication d'objet dans d'autre objets et de crée une boucle

--- condition.js
+++ condition.js
@@ -15,4 +15,3 @@ console.log(`Autorisé: ${authorised}`)                      //affichage de l'au
 
 var authorised2 = age >=18 ? true : false                   //  fonction des Condition ternaires
 console.log(`Autorisé: ${authorised}`)                      // affichage de l'autorisation des condition ternaires
-                                                            // a savoir qu'on est pas obliger d'avoir des accolades si on a qu'une ligne de

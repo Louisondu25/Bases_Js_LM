949162f3940eed564aea9a91ed92370aa5be802e
Louisondu25 <louisondu25@gmail.com>
2024-05-21 13:25:45 +0200

    Exercice sur les ids

--- seller.js
+++ seller.js
@@ -146,3 +146,113 @@ users = users.map(function (e) {
     return { ...e, articles: [] }; // Si random\_id\_number est égal à 0, ajoute une propriété "articles" à l'utilisateur avec un tableau vide
   }
 });
+//Tableau utilisateur de 25 elements les proriétés sont :
+// username - firstName - lastName - email
+
+// Ajouter une propriete aux utilisateurs "articles" [id.articles] un nombre aléatoire 
+// sans que une un utilisateur est le meme articles qu'un autre
+
+//console.log(articles)
+//articles = []
+
+// On itère sur chaque utilisateur de la variable users
+users = users.map(function (e) {
+  // On vérifie si la propriété articles de l'utilisateur actuel contient des éléments
+  if (e.articles.length > 0) {
+    // On crée une variable depenses qui contiendra les prix totaux TTC de chaque article acheté par l'utilisateur actuel
+    var depenses = e.articles.map(function (article) {
+      // Pour chaque article acheté par l'utilisateur actuel, on recherche l'article dans la variable articles en utilisant la méthode _.find de la bibliothèque Lodash
+      var a = _.find(articles, ["id", article])
+      // Si l'article est trouvé et qu'il a une propriété price_total_ttc, on ajoute le prix total TTC de l'article à la variable depenses. Sinon, on ajoute 0 à la variable depenses
+      if (a && a['price_total_ttc']) {
+        return a['price_total_ttc']
+      }
+      return 0
+    })
+    // On calcule le montant total dépensé par l'utilisateur actuel en sommant les prix totaux TTC de chaque article acheté en utilisant la méthode _.sum de la bibliothèque Lodash
+    var price_depense = _.sum(depenses)
+    // On ajoute la propriété depenses à l'utilisateur actuel avec la valeur du montant total dépensé
+    e["depenses"] = price_depense
+  }
+  // On renvoie l'utilisateur actuel avec ses propriétés modifiées
+  return e
+})
+// On itère sur chaque utilisateur de la variable users en utilisant la méthode map qui renvoie un nouveau tableau avec les utilisateurs modifiés
+
+//console.log(users)
+
+// articles = articles.map(function (article) {
+//   // On applique une fonction à chaque article du tableau articles
+//   for (var i = 0; i < users.length; i++) {
+//     // Pour chaque article, on parcourt la liste des utilisateurs
+//     var user = users[i];
+//     // On stocke l'utilisateur courant dans la variable user
+//     if (_.indexOf(user.articles, article.id) > -1) {
+//       // On vérifie si l'identifiant de l'article est dans la liste des articles de l'utilisateur
+//       article.user_id = user.id;
+//       // Si oui, on associe l'identifiant de l'utilisateur à l'article
+//     }
+//   }
+//   return article;
+//   // On renvoie l'article modifié
+// });
+
+// On itère sur chaque utilisateur du tableau users
+users.forEach(function (user) {
+    // Pour chaque utilisateur, on itère sur chaque article de son tableau d'articles
+    user.articles.forEach(function (user_article) {
+        // On recherche l'index de l'article dans le tableau articles en utilisant lodash's findIndex
+        var index = _.findIndex(articles, ["id", user_article])
+        // Si l'article est trouvé dans le tableau articles
+        if (index > -1) {
+            // On associe l'identifiant de l'utilisateur à l'article
+            articles[index].user_id = user.id
+        }
+    })
+})
+
+// On initialise un objet vide pour stocker les utilisateurs
+var obj_users = {}
+
+// On initialise un objet vide pour stocker les articles
+var obj_articles = {}
+
+// On itère sur chaque article du tableau articles
+// articles.forEach(function (article) {
+//     // On vérifie si l'article a un identifiant d'utilisateur
+//     if (article.user_id) {
+//         // Si l'utilisateur n'existe pas encore dans l'objet obj_users
+//         if (!obj_users[article.user_id])
+//             // On crée un tableau vide pour stocker les articles de l'utilisateur
+//             obj_users[article.user_id] = []
+//         // On ajoute l'article au tableau des articles de l'utilisateur
+//         obj_users[article.user_id].push(article)
+//     }
+// })
+
+// On utilise lodash's groupBy pour regrouper les articles par identifiant d'utilisateur
+obj_articles = _.groupBy(articles, 'user_id')
+
+
+
+// On utilise lodash's map pour itérer sur les clés de l'objet obj_articles
+// On retourne le nombre d'articles pour chaque utilisateur
+console.log(_.map(Object.keys(obj_articles), (e) => {
+    return obj_articles[e].length+ "-"+e
+}))
+
+// On définit l'identifiant de l'utilisateur sélectionné
+var user_id_select = "112"
+
+// Si l'utilisateur sélectionné a des articles dans l'objet obj_articles
+if (obj_articles[user_id_select]) {
+    // On affiche le nombre d'articles achetés par l'utilisateur et les noms des articles
+    console.log(`L'utilisateur ${user_id_select} à acheté `, obj_articles[user_id_select].length, "article(s)",`(${_.map(obj_articles[user_id_select], 'name').join(',')})` )
+}
+// Sinon, on affiche que l'utilisateur n'a rien acheté
+else {
+    console.log(`L'utilisateur ${user_id_select} n'a rien acheté.`)
+}
+
+// On affiche le tableau d'articles
+//console.log(articles)
